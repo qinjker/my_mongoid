093d80883b4c885c28b7f904c1ef92bf26c8cd22
qinjker <qinjker@gmail.com>
2014-02-18 15:29:42 +0800

    fix bug

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -3,28 +3,28 @@ require "my_mongoid/version"
 
 module MyMongoid
 	def self.models
-			@models ||= []
-		end
-	
-		def self.register_model(klass)
-			models.push klass if !models.include?(klass)
-		end
+		@models ||= []
 	end
-	
-	module MyMongoid::Document
-		def self.included(klass)
-			klass.module_eval do
-				extend ClassMethods
-				#field :_id, :as => :id
-				MyMongoid.register_model(klass)
-			end
-		end
-	
+
+	def self.register_model(klass)
+		models.push klass if !models.include?(klass)
 	end
-	
-	module MyMongoid::Document::ClassMethods
-		def is_mongoid_model?
-			true
+end
+
+class MyMongoid::Field
+end
+
+module MyMongoid::Document
+	def self.included(klass)
+		klass.module_eval do
+			extend ClassMethods
+			MyMongoid.register_model(klass)
 		end
-	
+	end	
+end
+
+module MyMongoid::Document::ClassMethods
+	def is_mongoid_model?
+		true
+	end
 end
